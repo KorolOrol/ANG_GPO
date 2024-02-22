bbdb1c68e230cd908b9402d41c47dc05d6a24113
Mikhail Zhukovich <zhukovich.m.572-2@e.tusur.ru>
2024-02-22 10:10:57 +0700

    feature(Model): Added base classes with properties feature(IGenerator): Added interface for generating

---
+++ src/base/BaseClasses/Model/Character.cs
@@ -0,0 +1,43 @@
+﻿namespace BaseClasses.Model
+{
+    /// <summary>
+    /// Персонаж истории
+    /// </summary>
+    public class Character
+    {
+        /// <summary>
+        /// Имя персонажа
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание персонажа
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Характер персонажа
+        /// </summary>
+        public string Temper { get; set; }
+
+        /// <summary>
+        /// Отношения персонажа с другими персонажами
+        /// </summary>
+        public Dictionary<Character, double> Relations { get; set; }
+
+        /// <summary>
+        /// Местоположение персонажа
+        /// </summary>
+        public List<Location> Locations { get; set; }
+
+        /// <summary>
+        /// Вещи персонажа
+        /// </summary>
+        public List<Item> Items { get; set; }
+
+        /// <summary>
+        /// События, в которых участвует персонаж
+        /// </summary>
+        public List<Event> Events { get; set; }
+    }
+}
---
+++ src/base/BaseClasses/Model/Item.cs
@@ -0,0 +1,33 @@
+﻿namespace BaseClasses.Model
+{
+    /// <summary>
+    /// Предмет в истории
+    /// </summary>
+    public class Item
+    {
+        /// <summary>
+        /// Название предмета
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание предмета
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Хозяин предмета
+        /// </summary>
+        public Character Host { get; set; }
+
+        /// <summary>
+        /// Локация, в которой находится предмет
+        /// </summary>
+        public Location Location { get; set; }
+
+        /// <summary>
+        /// События, в которых участвует предмет
+        /// </summary>
+        public List<Event> Events { get; set; }
+    }
+}
