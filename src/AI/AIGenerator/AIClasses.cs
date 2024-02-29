a46583d7e0e6e8caee066f2ab5034e6b9d2434dc
Mikhail Zhukovich <zhukovich.m.572-2@e.tusur.ru>
2024-02-29 10:10:30 +0700

    feature(AIClasses): temporal classes for corventing to base classes refactor(AIGenerator): added comments

---
+++ src/AI/AIGenerator/AIClasses.cs
@@ -0,0 +1,306 @@
+﻿using BaseClasses.Model;
+
+namespace AIGenerator
+{
+    /// <summary>
+    /// Персонаж истории, полученный от ИИ
+    /// </summary>
+    public class AICharacter
+    {
+        /// <summary>
+        /// Имя персонажа
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание персонажа
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Характер персонажа
+        /// </summary>
+        public string Temper { get; set; }
+
+        /// <summary>
+        /// Отношения персонажа с другими персонажами
+        /// </summary>
+        public Dictionary<string, double> Relations { get; set; }
+
+        /// <summary>
+        /// Местоположение персонажа
+        /// </summary>
+        public List<string> Locations { get; set; }
+
+        /// <summary>
+        /// Вещи персонажа
+        /// </summary>
+        public List<string> Items { get; set; }
+
+        /// <summary>
+        /// События, в которых участвует персонаж
+        /// </summary>
+        public List<string> Events { get; set; }
+
+        /// <summary>
+        /// Преобразование в стандартный класс персонажа
+        /// </summary>
+        /// <param name="characters">Список персонажей</param>
+        /// <param name="locations">Список локаций</param>
+        /// <param name="items">Список предметов</param>
+        /// <param name="events">Список событий</param>
+        /// <returns>Стандартный класс персонажа</returns>
+        /// <exception cref="ArgumentNullException">ИИ вернул пустой ответ</exception>
+        public Character ToCharacter(List<Character> characters, List<Location> locations, 
+                                     List<Item> items, List<Event> events)
+        {
+            if (this == null)
+            {
+                throw new ArgumentNullException("ИИ вернул пустой ответ");
+            }
+            Character character = new Character();
+            character.Name = Name;
+            character.Description = Description;
+            character.Temper = Temper;
+            character.Relations = new Dictionary<Character, double>();
+            foreach (var relation in Relations)
+            {
+                Character rel = characters.First(c => c.Name == relation.Key);
+                character.Relations.Add(rel, relation.Value);
+                rel.Relations.Add(character, relation.Value);
+            }
+            character.Locations = new List<Location>();
+            foreach (var location in Locations)
+            {
+                Location loc = locations.First(l => l.Name == location);
+                character.Locations.Add(loc);
+                loc.Characters.Add(character);
+            }
+            character.Items = new List<Item>();
+            foreach (var item in Items)
+            {
+                Item foundItem = items.First(i => i.Name == item);
+                character.Items.Add(foundItem);
+                foundItem.Host = character;
+            }
+            character.Events = new List<Event>();
+            foreach (var ev in Events)
+            {
+                Event @event = events.First(e => e.Name == ev);
+                character.Events.Add(@event);
+                @event.Characters.Add(character);
+            }
+            return character;
+        }
+    }
+
+    /// <summary>
+    /// Локация в истории, полученная от ИИ
+    /// </summary>
+    public class AILocation
+    {
+        /// <summary>
+        /// Название локации
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание локации
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Персонажи, находящиеся в локации
+        /// </summary>
+        public List<string> Characters { get; set; }
+
+        /// <summary>
+        /// Вещи, находящиеся в локации
+        /// </summary>
+        public List<string> Items { get; set; }
+
+        /// <summary>
+        /// События, происходящие в локации
+        /// </summary>
+        public List<string> Events { get; set; }
+
+        /// <summary>
+        /// Преобразование в стандартный класс локации
+        /// </summary>
+        /// <param name="characters">Список персонажей</param>
+        /// <param name="locations">Список локаций</param>
+        /// <param name="items">Список предметов</param>
+        /// <param name="events">Список событий</param>
+        /// <returns>Стандартный класс локации</returns>
+        /// <exception cref="ArgumentNullException">ИИ вернул пустой ответ</exception>
+        public Location ToLocation(List<Character> characters, List<Location> locations, 
+                                   List<Item> items, List<Event> events)
+        {
+            if (this == null)
+            {
+                throw new ArgumentNullException("ИИ вернул пустой ответ");
+            }
+            Location location = new Location();
+            location.Name = Name;
+            location.Description = Description;
+            location.Characters = new List<Character>();
+            foreach (var character in Characters)
+            {
+                Character charac = characters.First(c => c.Name == character);
+                location.Characters.Add(charac);
+                charac.Locations.Add(location);
+            }
+            location.Items = new List<Item>();
+            foreach (var item in Items)
+            {
+                Item foundItem = items.First(i => i.Name == item);
+                location.Items.Add(foundItem);
+                foundItem.Location = location;
+            }
+            location.Events = new List<Event>();
+            foreach (var ev in Events)
+            {
+                Event @event = events.First(e => e.Name == ev);
+                location.Events.Add(@event);
+                @event.Locations.Add(location);
+            }
+            return location;
+        }
+    }
+
+    /// <summary>
+    /// Предмет в истории, полученный от ИИ
+    /// </summary>
+    public class AIItem
+    {
+        /// <summary>
+        /// Название предмета
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание предмета
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Местоположение предмета
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Хозяин предмета
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// События, в которых участвует предмет
+        /// </summary>
+        public List<string> Events { get; set; }
+
+        /// <summary>
+        /// Преобразование в стандартный класс предмета
+        /// </summary>
+        /// <param name="characters">Список персонажей</param>
+        /// <param name="locations">Список локаций</param>
+        /// <param name="items">Список предметов</param>
+        /// <param name="events">Список событий</param>
+        /// <returns>Стандартный класс предмета</returns>
+        /// <exception cref="ArgumentNullException">ИИ вернул пустой ответ</exception>
+        public Item ToItem(List<Character> characters, List<Location> locations, 
+                           List<Item> items, List<Event> events)
+        {
+            if (this == null)
+            {
+                throw new ArgumentNullException("ИИ вернул пустой ответ");
+            }
+            Item item = new Item();
+            item.Name = Name;
+            item.Description = Description;
+            item.Location = locations.First(l => l.Name == Location);
+            item.Host = characters.First(c => c.Name == Host);
+            item.Events = new List<Event>();
+            foreach (var ev in Events)
+            {
+                Event @event = events.First(e => e.Name == ev);
+                item.Events.Add(@event);
+                @event.Items.Add(item);
+            }
+            return item;
+        }
+    }
+
+    /// <summary>
+    /// Событие в истории, полученное от ИИ
+    /// </summary>
+    public class AIEvent
+    {
+        /// <summary>
+        /// Название события
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание события
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Персонажи, участвующие в событии
+        /// </summary>
+        public List<string> Characters { get; set; }
+
+        /// <summary>
+        /// Локации, в которых происходит событие
+        /// </summary>
+        public List<string> Locations { get; set; }
+
+        /// <summary>
+        /// Вещи, участвующие в событии
+        /// </summary>
+        public List<string> Items { get; set; }
+
+        /// <summary>
+        /// Преобразование в стандартный класс события
+        /// </summary>
+        /// <param name="characters">Список персонажей</param>
+        /// <param name="locations">Список локаций</param>
+        /// <param name="items">Список предметов</param>
+        /// <param name="events">Список событий</param>
+        /// <returns>Стандартный класс события</returns>
+        /// <exception cref="ArgumentNullException">ИИ вернул пустой ответ</exception>
+        public Event ToEvent(List<Character> characters, List<Location> locations, 
+                             List<Item> items, List<Event> events)
+        {
+            if (this == null)
+            {
+                throw new ArgumentNullException("ИИ вернул пустой ответ");
+            }
+            Event @event = new Event();
+            @event.Name = Name;
+            @event.Description = Description;
+            @event.Characters = new List<Character>();
+            foreach (var character in Characters)
+            {
+                Character charac = characters.First(c => c.Name == character);
+                @event.Characters.Add(charac);
+                charac.Events.Add(@event);
+            }
+            @event.Locations = new List<Location>();
+            foreach (var location in Locations)
+            {
+                Location loc = locations.First(l => l.Name == location);
+                @event.Locations.Add(loc);
+                loc.Events.Add(@event);
+            }
+            @event.Items = new List<Item>();
+            foreach (var item in Items)
+            {
+                Item foundItem = items.First(i => i.Name == item);
+                @event.Items.Add(foundItem);
+                foundItem.Events.Add(@event);
+            }
+            return @event;
+        }
+    }
+}
--- src/AI/AIGenerator/AIGenerator.cs
+++ src/AI/AIGenerator/AIGenerator.cs
@@ -4,30 +4,65 @@ using Newtonsoft.Json;
 
 namespace AIGenerator
 {
+    /// <summary>
+    /// ИИ-генератор
+    /// </summary>
     public class AIGenerator : IGenerator
     {
+        /// <summary>
+        /// Генератор текста
+        /// </summary>
         public TextAIGenerator TextAIGenerator { get; set; }
 
+        /// <summary>
+        /// Системные подсказки
+        /// </summary>
         public Dictionary<string, string> SystemPrompt { get; set; }
 
+        /// <summary>
+        /// Загрузка системных подсказок
+        /// </summary>
+        /// <param name="path">Путь к файлу с подсказками</param>
         public void LoadSystemPrompt(string path)
         {
-            SystemPrompt = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(path));
+            SystemPrompt = 
+                JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(path));
         }
 
+        /// <summary>
+        /// Конструктор со стандартным ИИ
+        /// </summary>
+        /// <param name="promptPath">Путь к файлу с подсказками</param>
         public AIGenerator(string promptPath)
         {
             LoadSystemPrompt(promptPath);
             TextAIGenerator = new TextAIGenerator();
         }
 
+        /// <summary>
+        /// Конструктор с пользовательским ИИ
+        /// </summary>
+        /// <param name="promptPath">Путь к файлу с подсказками</param>
+        /// <param name="apiKey">Переменная среды с ключом API</param>
+        /// <param name="endpoint">Переменная среды с конечной точкой</param>
         public AIGenerator(string promptPath, string apiKey, string endpoint)
         {
             LoadSystemPrompt(promptPath);
             TextAIGenerator = new TextAIGenerator(apiKey, endpoint);
         }
 
-        private List<string> GetPromptForResponse(string task, List<Character>? characters, List<Location>? locations, List<Item>? items, List<Event>? events)
+        /// <summary>
+        /// Получение подсказок для генерации
+        /// </summary>
+        /// <param name="task">Необходимый элемент истории</param>
+        /// <param name="characters">Список персонажей</param>
+        /// <param name="locations">Список локаций</param>
+        /// <param name="items">Список предметов</param>
+        /// <param name="events">Список событий</param>
+        /// <returns>Список подсказок</returns>
+        private List<string> GetPromptForResponse(string task, List<Character>? characters, 
+                                                  List<Location>? locations, List<Item>? items, 
+                                                  List<Event>? events)
         {
             List<string> prompts = new List<string>
             {
@@ -79,32 +114,88 @@ namespace AIGenerator
             return prompts;
         }
 
-        public async Task<Character> GenerateCharacterAsync(List<Character>? characters = null, List<Location>? locations = null, List<Item>? items = null, List<Event>? events = null)
+        /// <summary>
+        /// Генерация персонажа
+        /// </summary>
+        /// <param name="characters">Персонажи, которые уже есть в истории</param>
+        /// <param name="locations">Локации, которые уже есть в истории</param>
+        /// <param name="items">Предметы, которые уже есть в истории</param>
+        /// <param name="events">События, которые уже есть в истории</param>
+        /// <returns>Сгенерированный персонаж</returns>
+        public async Task<Character> GenerateCharacterAsync(List<Character>? characters = null, 
+                                                            List<Location>? locations = null, 
+                                                            List<Item>? items = null, 
+                                                            List<Event>? events = null)
         {
             List<string> prompts = GetPromptForResponse("Character", characters, locations, items, events);
             string response = await TextAIGenerator.GenerateText(prompts);
-            return JsonConvert.DeserializeObject<Character>(response);
+            return JsonConvert.DeserializeObject<AICharacter>(response).ToCharacter(characters, 
+                                                                                    locations, 
+                                                                                    items, 
+                                                                                    events);
         }
 
-        public async Task<Location> GenerateLocation(List<Character>? characters = null, List<Location>? locations = null, List<Item>? items = null, List<Event>? events = null)
+        /// <summary>
+        /// Генерация локации
+        /// </summary>
+        /// <param name="characters">Персонажи, которые уже есть в истории</param>
+        /// <param name="locations">Локации, которые уже есть в истории</param>
+        /// <param name="items">Предметы, которые уже есть в истории</param>
+        /// <param name="events">События, которые уже есть в истории</param>
+        /// <returns>Сгенерированная локация</returns>
+        public async Task<Location> GenerateLocation(List<Character>? characters = null, 
+                                                     List<Location>? locations = null, 
+                                                     List<Item>? items = null, 
+                                                     List<Event>? events = null)
         {
             List<string> prompts = GetPromptForResponse("Location", characters, locations, items, events);
             string response = await TextAIGenerator.GenerateText(prompts);
-            return JsonConvert.DeserializeObject<Location>(response);
+            return JsonConvert.DeserializeObject<AILocation>(response).ToLocation(characters,
+                                                                                  locations,
+                                                                                  items,
+                                                                                  events);
         }
 
-        public async Task<Item> GenerateItem(List<Character>? characters = null, List<Location>? locations = null, List<Item>? items = null, List<Event>? events = null)
+        /// <summary>
+        /// Генерация предмета
+        /// </summary>
+        /// <param name="characters">Персонажи, которые уже есть в истории</param>
+        /// <param name="locations">Локации, которые уже есть в истории</param>
+        /// <param name="items">Предметы, которые уже есть в истории</param>
+        /// <param name="events">События, которые уже есть в истории</param>
+        /// <returns>Сгенерированный предмет</returns>
+        public async Task<Item> GenerateItem(List<Character>? characters = null, 
+                                             List<Location>? locations = null, 
+                                             List<Item>? items = null, 
+                                             List<Event>? events = null)
         {
             List<string> prompts = GetPromptForResponse("Item", characters, locations, items, events);
             string response = await TextAIGenerator.GenerateText(prompts);
-            return JsonConvert.DeserializeObject<Item>(response);
+            return JsonConvert.DeserializeObject<AIItem>(response).ToItem(characters,
+                                                                          locations,
+                                                                          items,
+                                                                          events);
         }
 
-        public async Task<Event> GenerateEvent(List<Character>? characters = null, List<Location>? locations = null, List<Item>? items = null, List<Event>? events = null)
+        /// <summary>
+        /// Генерация события
+        /// </summary>
+        /// <param name="characters">Персонажи, которые уже есть в истории</param>
+        /// <param name="locations">Локации, которые уже есть в истории</param>
+        /// <param name="items">Предметы, которые уже есть в истории</param>
+        /// <param name="events">События, которые уже есть в истории</param>
+        /// <returns>Сгенерированное событие</returns>
+        public async Task<Event> GenerateEvent(List<Character>? characters = null, 
+                                               List<Location>? locations = null, 
+                                               List<Item>? items = null, 
+                                               List<Event>? events = null)
         {
             List<string> prompts = GetPromptForResponse("Event", characters, locations, items, events);
             string response = await TextAIGenerator.GenerateText(prompts);
-            return JsonConvert.DeserializeObject<Event>(response);
+            return JsonConvert.DeserializeObject<AIEvent>(response).ToEvent(characters,
+                                                                            locations,
+                                                                            items,
+                                                                            events);
         }
     }
 }
