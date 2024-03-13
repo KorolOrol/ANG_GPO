721eb868b40db9805e5d5c803ad920507aa17f37
Mikhail Zhukovich <zhukovich.m.572-2@e.tusur.ru>
2024-03-13 19:42:36 +0700

    feature(Plot): added plot class feature(IGenerator): used plot class

---
+++ src/base/BaseClasses/Model/Plot.cs
@@ -0,0 +1,54 @@
+﻿namespace BaseClasses.Model
+{
+    /// <summary>
+    /// История
+    /// </summary>
+    public class Plot
+    {
+        /// <summary>
+        /// Список персонажей
+        /// </summary>
+        public List<Character> Characters { get; set; } = new();
+
+        /// <summary>
+        /// Список локаций
+        /// </summary>
+        public List<Location> Locations { get; set; } = new();
+
+        /// <summary>
+        /// Список предметов
+        /// </summary>
+        public List<Item> Items { get; set; } = new();
+
+        /// <summary>
+        /// Список событий
+        /// </summary>
+        public List<Event> Events { get; set; } = new();
+
+        /// <summary>
+        /// Полная информация об истории
+        /// </summary>
+        /// <returns>Полная информация об истории</returns>
+        public string FullInfo()
+        {
+            string info = "";
+            foreach (var c in Characters)
+            {
+                info += c.FullInfo() + "\n";
+            }
+            foreach (var l in Locations)
+            {
+                info += l.FullInfo() + "\n";
+            }
+            foreach (var i in Items)
+            {
+                info += i.FullInfo() + "\n";
+            }
+            foreach (var e in Events)
+            {
+                info += e.FullInfo() + "\n";
+            }
+            return info;
+        }
+    }
+}
