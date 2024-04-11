11a11a3f392c3b7047dad4cf8ee46729d7b71809
Mikhail Zhukovich <zhukovich.m.572-2@e.tusur.ru>
2024-04-11 11:22:44 +0700

    feature(Binder): added Unbind feature(base classes): added Merge

--- src/base/BaseClasses/Services/Binder.cs
+++ src/base/BaseClasses/Services/Binder.cs
@@ -39,6 +39,25 @@ namespace BaseClasses.Services
         }
 
         /// <summary>
+        /// Разъединение двух персонажей
+        /// </summary>
+        /// <param name="character1">Первый персонаж</param>
+        /// <param name="character2">Второй персонаж</param>
+        public static void Unbind(Character character1, Character character2)
+        {
+            Relation rel1 = character1.Relations.FirstOrDefault(rel => rel.Character == character2);
+            Relation rel2 = character2.Relations.FirstOrDefault(rel => rel.Character == character1);
+            if (rel1 != null)
+            {
+                character1.Relations.Remove(rel1);
+            }
+            if (rel2 != null)
+            {
+                character2.Relations.Remove(rel2);
+            }
+        }
+
+        /// <summary>
         /// Связывание персонажа с локацией
         /// </summary>
         /// <param name="character">Персонаж</param>
@@ -52,6 +71,23 @@ namespace BaseClasses.Services
             if (!location.Characters.Contains(character))
             {
                 location.Characters.Add(character);
+            }
+        }
+
+        /// <summary>
+        /// Разъединение персонажа с локацией
+        /// </summary>
+        /// <param name="character">Персонаж</param>
+        /// <param name="location">Локация</param>
+        public static void Unbind(Character character, Location location)
+        {
+            if (character.Locations.Contains(location))
+            {
+                character.Locations.Remove(location);
+            }
+            if (location.Characters.Contains(character))
+            {
+                location.Characters.Remove(character);
             }
         }
 
@@ -74,6 +110,23 @@ namespace BaseClasses.Services
         }
 
         /// <summary>
+        /// Разъединение персонажа с предметом
+        /// </summary>
+        /// <param name="character">Персонаж</param>
+        /// <param name="item">Предмет</param>
+        public static void Unbind(Character character, Item item)
+        {
+            if (character.Items.Contains(item))
+            {
+                character.Items.Remove(item);
+            }
+            if (item.Host == character)
+            {
+                item.Host = null;
+            }
+        }
+
+        /// <summary>
         /// Связывание персонажа с событием
         /// </summary>
         /// <param name="character">Персонаж</param>
@@ -91,6 +144,23 @@ namespace BaseClasses.Services
         }
 
         /// <summary>
+        /// Разъединение персонажа с событием
+        /// </summary>
+        /// <param name="character">Персонаж</param>
+        /// <param name="event">Событие</param>
+        public static void Unbind(Character character, Event @event)
+        {
+            if (character.Events.Contains(@event))
+            {
+                character.Events.Remove(@event);
+            }
+            if (@event.Characters.Contains(character))
+            {
+                @event.Characters.Remove(character);
+            }
+        }
+
+        /// <summary>
         /// Связывание локации с персонажем
         /// </summary>
         /// <param name="location">Локация</param>
@@ -98,6 +168,16 @@ namespace BaseClasses.Services
         public static void Bind(Location location, Character character)
         {
             Bind(character, location);
+        }
+
+        /// <summary>
+        /// Разъединение локации с персонажем
+        /// </summary>
+        /// <param name="location">Локация</param>
+        /// <param name="character">Персонаж</param>
+        public static void Unbind(Location location, Character character)
+        {
+            Unbind(character, location);
         }
 
         /// <summary>
@@ -119,6 +199,23 @@ namespace BaseClasses.Services
         }
 
         /// <summary>
+        /// Разъединение локации с предметом
+        /// </summary>
+        /// <param name="location">Локация</param>
+        /// <param name="item">Предмет</param>
+        public static void Unbind(Location location, Item item)
+        {
+            if (location.Items.Contains(item))
+            {
+                location.Items.Remove(item);
+            }
+            if (item.Location == location)
+            {
+                item.Location = null;
+            }
+        }
+
+        /// <summary>
         /// Связывание локации с событием
         /// </summary>
         /// <param name="location">Локация</param>
@@ -136,6 +233,23 @@ namespace BaseClasses.Services
         }
 
         /// <summary>
+        /// Разъединение локации с событием
+        /// </summary>
+        /// <param name="location">Локация</param>
+        /// <param name="event">Событие</param>
+        public static void Unbind(Location location, Event @event)
+        {
+            if (location.Events.Contains(@event))
+            {
+                location.Events.Remove(@event);
+            }
+            if (@event.Locations.Contains(location))
+            {
+                @event.Locations.Remove(location);
+            }
+        }
+
+        /// <summary>
         /// Связывание предмета с персонажем
         /// </summary>
         /// <param name="item">Предмет</param>
@@ -146,6 +260,16 @@ namespace BaseClasses.Services
         }
 
         /// <summary>
+        /// Разъединение предмета с персонажем
+        /// </summary>
+        /// <param name="item">Предмет</param>
+        /// <param name="character">Персонаж</param>
+        public static void Unbind(Item item, Character character)
+        {
+            Unbind(character, item);
+        }
+
+        /// <summary>
         /// Связывание предмета с локацией
         /// </summary>
         /// <param name="item">Предмет</param>
@@ -153,6 +277,16 @@ namespace BaseClasses.Services
         public static void Bind(Item item, Location location)
         {
             Bind(location, item);
+        }
+
+        /// <summary>
+        /// Разъединение предмета с локацией
+        /// </summary>
+        /// <param name="item">Предмет</param>
+        /// <param name="location">Локация</param>
+        public static void Unbind(Item item, Location location)
+        {
+            Unbind(location, item);
         }
 
         /// <summary>
@@ -173,6 +307,23 @@ namespace BaseClasses.Services
         }
 
         /// <summary>
+        /// Разъединение предмета с событием
+        /// </summary>
+        /// <param name="item">Предмет</param>
+        /// <param name="event">Событие</param>
+        public static void Unbind(Item item, Event @event)
+        {
+            if (item.Events.Contains(@event))
+            {
+                item.Events.Remove(@event);
+            }
+            if (@event.Items.Contains(item))
+            {
+                @event.Items.Remove(item);
+            }
+        }
+
+        /// <summary>
         /// Связывание события с персонажем
         /// </summary>
         /// <param name="event"></param>
@@ -180,6 +331,16 @@ namespace BaseClasses.Services
         public static void Bind(Event @event, Character character)
         {
             Bind(character, @event);
+        }
+        
+        /// <summary>
+        /// Разъединение события с персонажем
+        /// </summary>
+        /// <param name="event">Событие</param>
+        /// <param name="character">Персонаж</param>
+        public static void Unbind(Event @event, Character character)
+        {
+            Unbind(character, @event);
         }
 
         /// <summary>
@@ -193,6 +354,16 @@ namespace BaseClasses.Services
         }
 
         /// <summary>
+        /// Разъединение события с локацией
+        /// </summary>
+        /// <param name="event">Событие</param>
+        /// <param name="location">Локация</param>
+        public static void Unbind(Event @event, Location location)
+        {
+            Unbind(location, @event);
+        }
+
+        /// <summary>
         /// Связывание события с предметом
         /// </summary>
         /// <param name="event">Событие</param>
@@ -200,6 +371,16 @@ namespace BaseClasses.Services
         public static void Bind(Event @event, Item item)
         {
             Bind(item, @event);
+        }
+
+        /// <summary>
+        /// Разъединение события с предметом
+        /// </summary>
+        /// <param name="event">Событие</param>
+        /// <param name="item">Предмет</param>
+        public static void Unbind(Event @event, Item item)
+        {
+            Unbind(item, @event);
         }
     }
 }
