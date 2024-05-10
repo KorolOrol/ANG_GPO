226edd17902648c617788a79f656379ae6946e52
Mikhail Zhukovich <zhukovich.m.572-2@e.tusur.ru>
2024-05-11 03:39:05 +0700

    refactor(all_sln): removed unused using, added comments

--- src/base/BaseClasses/Services/Binder.cs
+++ src/base/BaseClasses/Services/Binder.cs
@@ -1,10 +1,5 @@
 ﻿using BaseClasses.Interface;
 using BaseClasses.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BaseClasses.Services
 {
