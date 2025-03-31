77993b1bc81313cd9dd70a2f4d8b09e7d5b11284
KorolOrol <muxanuk.67@gmail.com>
2025-03-31 19:01:05 +0700

    tests(SerializerTests): добавлен класс для тестов Serializer

---
+++ src/base/BaseClasses.Tests/Services/SerializerTests.cs
@@ -0,0 +1,37 @@
+﻿using BaseClasses.Interface;
+using BaseClasses.Model;
+using BaseClasses.Enum;
+using BaseClasses.Services;
+
+namespace BaseClasses.Tests.Services
+{
+    /// <summary>
+    /// Тесты для класса Serializer
+    /// </summary>
+    public class SerializerTests
+    {
+        /// <summary>
+        /// Тесты для класса Serializer. Сохранение и загрузка элементов истории без связей.
+        /// </summary>
+        public class SerializerSingleElementsTests
+        {
+            /// <summary>
+            /// Сохранение персонажа без связей с другими элементами.
+            /// Ожидание: файл с верными данными персонажа.
+            /// </summary>
+            [Fact]
+            public void Serialize_CharacterWithoutBonds_CorrectFile()
+            {
+                // Arrange
+                var character = new Element(ElemType.Character, "Name", "Description");
+                character.Params.Add("Traits", new List<string> { "Trait1", "Trait2" });
+                // Act
+                Serializer.Serialize(character, "CharacterWithoutBonds.json");
+                // Assert
+                var expected = File.ReadAllText("CharacterWithoutBonds.json");
+                var actual = "";
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
