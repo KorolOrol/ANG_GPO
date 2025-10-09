69068ad6f499d78af3c137ef00f804bbb6f4e243
Mikhail Zhukovich <muxanuk.67@gmail.com>
2025-10-09 15:14:56 +0700

    Add new UI icons, fonts, and update main window layout
    Added SVG icons and new font assets. Updated MainWindow.uxml to use new icons and restructured header buttons. Introduced ElementListItem.uxml and updated MainWindowScript.cs to support dynamic element addition. Removed old PNG icons and expanded the NordColorPallete.uss. Updated SampleScene.unity to include a UIDocument and made related scene and settings adjustments.

--- src/Demo_app/Assets/UI/MainWindowScript.cs
+++ src/Demo_app/Assets/UI/MainWindowScript.cs
@@ -1,16 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MainWindowScript : MonoBehaviour
 {
+    [SerializeField] private VisualTreeAsset _viewAction;
+    [SerializeField] private VisualTreeAsset _elementListItem;
+    
+    private VisualElement root;
+    private TemplateContainer newAction;
+    private Button addButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        root = GetComponent<UIDocument>().rootVisualElement;
+        newAction = _viewAction.CloneTree();
+        var actionArea = root.Q<VisualElement>("ActionArea");
+        actionArea.Add(newAction);
+        addButton = root.Q<Button>("NewButton");
+        addButton.clicked += AddElement;
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void AddElement()
     {
-        
+        Debug.Log("Add element");
+        var elementsList = root.Q<ListView>("ElementsList");
+        var itemsSource = elementsList.itemsSource as List<string>;
+        if (itemsSource == null)
+        {
+            itemsSource = new List<string>();
+            elementsList.itemsSource = itemsSource;
+            elementsList.makeItem = () => _elementListItem.CloneTree();
+            elementsList.bindItem = (element, i) =>
+            {
+                // Пример: если в шаблоне есть Label с именем "Label"
+                var label = element.Q<Label>("Label");
+                if (label != null)
+                    label.text = itemsSource[i];
+            };
+        }
+        itemsSource.Add("Новый элемент");
+        elementsList.Rebuild();
     }
 }
