885014286add04d41445d0a11767e0aa26c31f2d
Mikhail Zhukovich <muxanuk.67@gmail.com>
2026-03-05 17:34:16 +0700

    feat: add AI upgrade functionality to ProppGenerator tests and include AIGenerator project reference

--- src/StructuredNarrative/StructuredNarrative.Tests/ProppGeneratorTests.cs
+++ src/StructuredNarrative/StructuredNarrative.Tests/ProppGeneratorTests.cs
@@ -1,4 +1,5 @@
-﻿using BaseClasses.Enum;
+﻿using AIGenerator;
+using BaseClasses.Enum;
 using BaseClasses.Model;
 using StructuredNarrative.Data;
 using StructuredNarrative.Model;
@@ -36,8 +37,48 @@ public class ProppGeneratorTests(ITestOutputHelper testOutputHelper)
             roledCharacter.Params.Add("ProppRole", role);
             plot.Add(roledCharacter);
         }
-
         await generator.GenerateChainAsync(plot, new Element(ElemType.Event));
         testOutputHelper.WriteLine(plot.FullInfo());
+    }
+    
+    [Fact]
+    public async Task GenerationWithAiUpgrade()
+    {
+        Plot plot = new();
+        ProppFunctionRegistry.Load("/home/korolorol/src/ANG_GPO/src/StructuredNarrative/Data/Propp.jsonl");
+        ProppGenerator proppGenerator = new() { SkipProbability = 0.8 };
+        foreach (var role in ProppFunctionRegistry.Roles)
+        {
+            var roledCharacter = new Element(ElemType.Character, role);
+            roledCharacter.Params.Add("ProppRole", role);
+            plot.Add(roledCharacter);
+        }
+        await proppGenerator.GenerateChainAsync(plot, new Element(ElemType.Event));
+        testOutputHelper.WriteLine(plot.FullInfo());
+        var llmAiGenerator =
+            new LlmAiGenerator("/home/korolorol/src/ANG_GPO/src/AI/AIGenerator/SystemPromptExample.json")
+                {
+                    TextAiGenerator =
+                    {
+                        Endpoint = "http://127.0.0.1:1234/v1",
+                        Model = "qwen3.5-4b"
+                    },
+                    AIPriority = true,
+                    UseStructuredOutput = true
+                };
+        for (int i = 0; i < plot.Elements.Count; i++)
+        {
+            try
+            {
+                var element = plot.Elements[i];
+                await llmAiGenerator.GenerateAsync(plot, element);
+                testOutputHelper.WriteLine($"Upgraded {i} from {plot.Elements.Count}:\n" + element.FullInfo());
+            }
+            catch (Exception ex)
+            {
+                testOutputHelper.WriteLine($"Error upgrading element {i}: {ex.Message}");
+                i--;
+            }
+        }
     }
 }
