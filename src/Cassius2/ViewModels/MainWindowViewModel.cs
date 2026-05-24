8797b227c945369a555bd36dcd987759e9b5afea
Mikhail Zhukovich <muxanuk.67@gmail.com>
2026-05-25 01:07:24 +0700

    add(App): добавлено начальное приложение с основными настройками и структурой add(svg): добавлены иконки для интерфейса

---
+++ src/Cassius2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Cassius2.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ src/Cassius2/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Cassius2.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
